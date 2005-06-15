f5d0874bc8fceb37d95cbaefde676de80206376c
Rafal Lindemann <rl@stamina.pl>
2005-06-15 10:42:25 +0000

    Keywords
    git-svn-id: http://www.konnekt.info/svn/staminalib/trunk@6 e7532c55-18f9-0310-997f-f942ee4ad645

--- Stamina/DataTable/DataTable.h
+++ Stamina/DataTable/DataTable.h
@@ -1,3 +1,13 @@
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2003,2004,2005 Rafa³ Lindemann, Stamina
+ *
+ *  $Id $
+ */
+
 #ifndef __DATATABLE__
 #define __DATATABLE__
 
