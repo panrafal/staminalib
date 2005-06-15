ba2892baa34a60b1f96647ba480a890aabe1e69e
Rafal Lindemann <rl@stamina.pl>
2005-06-15 10:42:25 +0000

    Keywords
    git-svn-id: http://www.konnekt.info/svn/staminalib/trunk/Stamina/DataTable@6 e7532c55-18f9-0310-997f-f942ee4ad645

--- DT.h
+++ DT.h
@@ -1,3 +1,12 @@
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2003,2004,2005 Rafa³ Lindemann, Stamina
+ *
+ *  $Id $
+ */
 
 /**@file All basic definitions needed to work with DataTable
 
---
+++ FileBase.h
@@ -0,0 +1,9 @@
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2003,2004,2005 Rafa³ Lindemann, Stamina
+ *
+ *  $Id $
+ */
