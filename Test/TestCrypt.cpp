48eb544fa6a2fa6ab33c076b05b874305fa1b968
Rafal Lindemann <rl@stamina.pl>
2005-06-21 13:17:50 +0000

    - Bardzo istotne poprawki w funkcji random - TestSuite - żeby uruchomić któryś z testów (znajdują się w katalogach Test\ poszczególnych bibliotek) wystarczy go dodać do projektu TestSuite, skompilować i uruchomić - Pierwszy działający test - TestCrypt
    git-svn-id: http://www.konnekt.info/svn/staminalib/trunk/Stamina/DataTable@21 e7532c55-18f9-0310-997f-f942ee4ad645

---
+++ Test/TestCrypt.cpp
@@ -0,0 +1,91 @@
+#include <stdafx.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "..\Crypt.h"
+#include <Stamina\MD5.h>
+#include <Stamina\Helpers.h>
+
+using namespace Stamina;
+using namespace Stamina::DT;
+
+const int timingLoops = 2000;
+
+class TestCrypt : public CPPUNIT_NS::TestFixture
+{
+  
+	CPPUNIT_TEST_SUITE( TestCrypt );
+  
+	CPPUNIT_TEST( testXor1Long );
+	CPPUNIT_TEST( testXor1Short );
+	CPPUNIT_TEST( testXor2Long );
+	CPPUNIT_TEST( testXor2Short );
+	CPPUNIT_TEST( testXor1Timing );
+	CPPUNIT_TEST( testXor2Timing );
+
+    CPPUNIT_TEST_SUITE_END();
+
+protected:
+  
+	unsigned char* _xor1_key;
+	Stamina::MD5Digest _xor2_key;
+
+	std::string _testStringLong;
+	std::string _testStringShort;
+
+public:
+	void setUp() {
+		_xor1_key = (unsigned char*)"abcdefghijklmno";
+		_xor2_key.calculate((char*)_xor1_key);
+		_testStringLong = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean eget arcu sed tellus dapibus pellentesque. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur ac ante sed ante laoreet congue. In volutpat, urna non sagittis ullamcorper, eros justo dignissim lectus, vitae fringilla lectus ligula quis turpis. Ut non turpis nec wisi rutrum laoreet. Mauris tincidunt odio sit amet mi. Sed adipiscing. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Aenean turpis. Sed rhoncus. Morbi facilisis, wisi vel nonummy luctus, ante tellus porta neque, sed fermentum velit arcu tempor purus. Sed rhoncus tortor eu lorem. Pellentesque ornare condimentum wisi. In hac habitasse platea dictumst. Proin porttitor, odio ut tristique feugiat, sapien ipsum rutrum sem, eget consequat nisl tellus in leo. Maecenas ut lorem. Quisque tristique lorem a eros. Etiam justo.";
+		_testStringShort = "\1\2\3";
+	}
+	void tearDown() {
+	}
+
+protected:
+
+	void testXor1Long() {
+		testXor1(_testStringLong);
+	}
+	void testXor1Short() {
+		testXor1(_testStringShort);
+	}
+	void testXor1Timing() {
+		for (int i = 0; i < timingLoops; i++)
+			testXor1(_testStringLong);
+	}
+	void testXor2Long() {
+		testXor2(_testStringLong);
+	}
+	void testXor2Short() {
+		testXor2(_testStringShort);
+	}
+	void testXor2Timing() {
+		for (int i = 0; i < timingLoops; i++)
+			testXor2(_testStringLong);
+	}
+
+	// koduje -> dekoduje
+	void testXor1(const std::string& testString) {
+		std::string encoded = testString; 
+		// xor1 nie zmienia d³ugoœci, wiêc w ten sposób bêdzie idealnie...
+		Stamina::DT::xor1_encrypt(_xor1_key, (unsigned char*)encoded.c_str(), encoded.length());
+		Stamina::DT::xor1_decrypt(_xor1_key, (unsigned char*)encoded.c_str(), encoded.length());
+		CPPUNIT_ASSERT_EQUAL(testString, encoded);
+	}
+
+	// koduje -> dekoduje
+	void testXor2(const std::string& testString) {
+		std::string encoded = testString; 
+		unsigned int salt = random(0, 0xFFFFFFFF);
+		// xor1 nie zmienia d³ugoœci, wiêc w ten sposób bêdzie idealnie...
+		Stamina::DT::xor2_encrypt(_xor2_key.getDigest(), (unsigned char*)encoded.c_str(), encoded.length(), salt);
+		Stamina::DT::xor2_decrypt(_xor2_key.getDigest(), (unsigned char*)encoded.c_str(), encoded.length(), salt);
+		CPPUNIT_ASSERT_EQUAL(testString, encoded);
+	}
+
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( TestCrypt );
+
+
