628a41dddb144a33895e99a23e053c5ed230e25c
Rafal Lindemann <rl@stamina.pl>
2005-07-05 15:28:02 +0000

    ConsoleProgress wydzielony do osobnego nagłówka...
    git-svn-id: http://www.konnekt.info/svn/staminalib/trunk@27 e7532c55-18f9-0310-997f-f942ee4ad645

---
+++ Stamina/Console.h
@@ -0,0 +1,47 @@
+#pragma once
+
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2003,2004,2005 Rafa³ Lindemann, Stamina
+ *
+ *  $Id: WinHelper.h 17 2005-06-18 12:52:05Z hao $
+ */
+
+#include <iostream>
+
+namespace Stamina {
+
+	class ConsoleProgress {
+	public:
+		ConsoleProgress(unsigned int from, unsigned int to, unsigned int width = 50) {
+			//		_from = from;
+			_to = to - from;
+			_pos = 0;
+			_width = width;
+			_bars = 0;
+		}
+		void drawBar() {
+			for (int i=0; i < _width; i++) {
+				std::cout << ".";
+			}
+		}
+		void doStep(unsigned int offset = 1) {
+			_pos += offset;
+			unsigned int newBars = ((float)_pos / _to) * _width;
+			while (_bars < newBars) {
+				std::cout << "#";
+				_bars++;
+			}
+		}
+	private:
+		unsigned int _from, _to;
+		unsigned int _pos;
+		unsigned int _width;
+		unsigned int _bars;
+
+	};
+
+};
