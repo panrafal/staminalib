6b98f44dd84c22ecf34911f64ea5c4d1964da445
Rafal Lindemann <rl@stamina.pl>
2005-07-17 14:24:12 +0000

    - Pierwsza kompilowalna wersja Stamina::Unique. Kod przeniesiony z rdzenia Konnekta jednak potrzebuje kilku testów dla pewności...
    git-svn-id: http://www.konnekt.info/svn/staminalib/trunk@47 e7532c55-18f9-0310-997f-f942ee4ad645

---
+++ Stamina/Unique.cpp
@@ -0,0 +1,140 @@
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2003,2004,2005 Rafa³ Lindemann, Stamina
+ *
+ *  $Id$
+ */
+
+#include "stdafx.h"
+#include "UniqueImpl.h"
+
+namespace Stamina { namespace Unique {
+
+	oDomainList domainList;
+
+	iDomainList* mainInstance() {
+		if (!domainList) {
+			domainList = new DomainList();
+		}
+		return domainList.get();
+	}
+
+
+	oRange basicRange(tRangeId rangeId, iRange::enType type, unsigned int priority,  unsigned int minimum, unsigned int maximum, unsigned int uniqueStart) {
+		return new Range(rangeId, type, priority, minimum, maximum, uniqueStart);
+	}
+
+	oDomain domainBasic(tDomainId id, const StringRef& name) {
+		return new Domain(id, name);
+	}
+
+	oDomainList domainListBasic() {
+		return new DomainList();
+	}
+
+// RANGE ----------------------------------------------------------------
+
+
+// DOMAIN --------------------------------------------------------------
+
+	tId Domain::getId(const StringRef& name) const {
+		tIdMap::const_iterator it = this->findId(name);
+		return it != this->_idMap.end() ? it->second : idNotFound;
+	}
+
+	String Domain::getName(tId id) const {
+		tIdMap::const_iterator it = this->findId(id);
+		return it != this->_idMap.end() ? it->first : "";
+	}
+
+	oRange Domain::inRange(tId id, Range::enType check) const {
+		oRange found;
+		for (tRanges::const_iterator it = _ranges.begin(); it != _ranges.end(); it++) {
+			if ((it->second->getType() & check) && (!found || found->getPriority() < it->second->getPriority())
+				&& it->second->containsId(id)) 
+			{
+				found = it->second;
+			}
+		}
+		return found;
+	}
+
+	Domain::tIdMap::iterator Domain::findId(tId id) {
+		tIdMap::iterator it;
+		for (it = _idMap.begin(); it != _idMap.end(); it++) {
+			if (it->second == id) break;
+		}
+		return it;
+	}
+
+	Domain::tIdMap::iterator Domain::findId(const StringRef& name) {
+		return this->_idMap.find(name);
+	}
+
+	Domain::tIdMap::const_iterator Domain::findId(tId id) const {
+		tIdMap::const_iterator it;
+		for (it = _idMap.begin(); it != _idMap.end(); it++) {
+			if (it->second == id) break;
+		}
+		return it;
+	}
+
+	Domain::tIdMap::const_iterator Domain::findId(const StringRef& name) const {
+		return this->_idMap.find(name);
+	}
+
+	bool Domain::registerId(tId id, const StringRef& name) {
+		if (id == idNotFound || this->idExists(id) || this->nameExists(name))
+			return false;
+		this->_idMap[name] = id;
+		return true;
+	}
+
+	tId Domain::registerName(const StringRef& name, const oRange& range) {
+		tId id = 0;
+		if ((id = this->getId(name)) != idNotFound) return id;
+		if (!range || !range->canRegisterName()) return idNotFound;
+		while ((id=range->createUniqueId()) != idNotFound) {
+			if (!this->idExists(id)) {
+				this->_idMap[name] = id;
+				break;
+			}
+		}
+		return id;
+	}
+
+	bool Domain::unregister(const StringRef& name) {
+		if (!this->nameExists(name)) return false;
+		this->_idMap.erase(name);
+		return true;
+	}
+
+	bool Domain::addRange(const oRange& range, bool setAsDefault) {
+		if (range->getRangeId()==rangeNotFound || this->rangeExists(range)) return false;
+		this->_ranges[range->getRangeId()] = range;
+		if (setAsDefault && range->getType() != Range::typeStatic)
+			this->_defaultRange = range;
+		return true;
+	}
+
+	oRange Domain::getRange(tRangeId rangeId) {
+		if (rangeId == rangeDefault)
+			return this->_defaultRange;
+		tRanges::iterator it = this->_ranges.find(rangeId);
+		return it == this->_ranges.end() ? oRange() : it->second;
+	}
+
+	bool Domain::removeRange(const oRange& range) {
+		if (!this->rangeExists(range)) 
+			return false;
+		this->_ranges.erase(range->getRangeId());
+		return true;
+	}
+
+
+
+
+};};
---
+++ Stamina/Unique.h
@@ -0,0 +1,161 @@
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2003,2004,2005 Rafa³ Lindemann, Stamina
+ *
+ *  $Id$
+ */
+
+#pragma once
+
+#include <string>
+#include <Stamina\Object.h>
+#include <Stamina\ObjectPtr.h>
+#include <Stamina\String.h>
+
+
+namespace Stamina { namespace Unique {
+
+	enum enDomainId {
+		domainOther,
+		domainDomain,
+		domainRange,
+		domainTable,
+		domainIMessage,
+		domainMessageType,
+		domainAction,
+		domainNet,
+		domainIcon,
+		domainNotFound = -1
+	};
+	enum enRangeId {
+		rangeMain = 0,
+		rangeStatic,
+		rangeUnique,
+		rangeUniqueLow,
+		rangeDefault = -1,
+		rangeNotFound = -1
+	};
+
+	typedef enDomainId tDomainId;
+	typedef enRangeId tRangeId;
+	typedef unsigned int tId;
+
+	const tId idNotFound = -1;
+	const tId commonUniqueStart = 0x00800000;
+
+	class iRange: public iSharedObject {
+	public:
+
+		STAMINA_OBJECT_CLASS(Stamina::Unique::iRange, iSharedObject);
+
+		/** Rodzaj zakresu		
+		*/
+		enum enType {
+			typeBoth = 3,
+			// Zakres tylko dla identyfikatorów statycznych
+			typeStatic = 1,
+			// Zakres tylko dla identyfikatorów unikalnych
+			typeUnique = 2
+		};
+
+		virtual tId __stdcall createUniqueId() =0;
+		virtual bool __stdcall canRegisterId(tId id) const =0;
+		virtual bool __stdcall canRegisterName() const =0;
+		virtual bool __stdcall containsId(tId id) const =0;
+
+		virtual tRangeId __stdcall getRangeId() const =0;
+		virtual unsigned int __stdcall getPriority() const =0;
+		virtual enType __stdcall getType() const =0;
+		virtual unsigned int __stdcall getMinimum() const =0;
+		virtual unsigned int __stdcall getMaximum() const =0;
+		virtual unsigned int __stdcall getNextUnique() const =0;
+
+	protected:
+	};
+
+	typedef SharedPtr<iRange> oRange;
+
+	/** Tworzy podstawowy zakres identyfikatorów
+	*
+	* \param id Unikalny identyfikator zakresu
+	* \param priority Priorytet zakresu okreœlaj¹cy jego szczegó³owoœæ. Bardziej ogólne zakresy powinny mieæ ni¿szy priorytet. Zakres domyœlny powinien mieæ priorytet 0.
+	* \param type Rodzaj zakresu
+	* \param minimum Minimalny identyfikator
+	* \param maximum Maxymalny identyfikator
+	* \param uniqueStart Pierwszy identyfikator do automatycznego nadawania. -1 ustawia ten parametr na wartoœæ @a minimum.
+	* \return 
+	*/
+	oRange basicRange(tRangeId rangeId, iRange::enType type, unsigned int priority,  unsigned int minimum = 1, unsigned int maximum = 0x7FFFFFFF, unsigned int uniqueStart=-1);
+
+
+	class iDomain: public iSharedObject {
+	public:
+
+		STAMINA_OBJECT_CLASS(Stamina::Unique::iDomain, iSharedObject);
+
+
+		virtual tDomainId __stdcall getDomainId() const =0;
+		virtual String __stdcall getDomainName() const =0;
+
+		virtual tId __stdcall getId(const StringRef& name) const =0;
+		virtual String __stdcall getName(tId id) const =0;
+		virtual oRange __stdcall inRange(tId id, iRange::enType check = iRange::typeBoth) const =0;
+		bool idExists(tId id) const {
+			return getName(id).empty() == false;
+		}
+		bool nameExists(const StringRef& name) const {
+			return getId(name) != idNotFound;
+		}
+
+		virtual bool __stdcall registerId(tId id, const StringRef& name) =0;
+		virtual tId __stdcall registerName(const StringRef& name, const oRange& range);
+		tId registerName(const StringRef& name, tRangeId range) {
+			return registerName(name, getRange(range));
+		}
+		virtual bool __stdcall unregister(tId id) =0;
+		virtual bool __stdcall unregister(const StringRef& name) =0;
+
+		virtual bool __stdcall addRange(const oRange& range, bool setAsDefault = false) =0;
+		bool rangeExists(tRangeId rangeId) {
+			return getRange(rangeId) == true;
+		}
+		bool rangeExists(const oRange& range) {
+			return rangeExists(range->getRangeId());
+		}
+		virtual oRange __stdcall getRange(tRangeId rangeId) =0;
+        virtual bool __stdcall removeRange(const oRange& range) =0;
+
+	};
+
+
+	typedef SharedPtr<iDomain> oDomain;
+
+	oDomain domainBasic(tDomainId id, const StringRef& name);
+
+
+	class iDomainList: public iSharedObject {
+	public:
+
+		STAMINA_OBJECT_CLASS(Stamina::Unique::iDomainList, iSharedObject);
+
+
+		virtual oDomain __stdcall getDomain(tDomainId id) const =0;
+		virtual void __stdcall registerDomain(const oDomain& domain) =0;
+		virtual void __stdcall unregisterDomain(const oDomain& domain) =0;
+
+		bool domainExists(tDomainId id) const {
+			return getDomain(id) == true;
+		}
+
+	};
+
+	typedef SharedPtr<iDomainList> oDomainList;
+
+	oDomainList domainListBasic();
+
+	iDomainList* mainInstance();
+
+};};
