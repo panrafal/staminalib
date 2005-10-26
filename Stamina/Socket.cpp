3792b2ad2695d8817acbf102097768197f67be26
Krzysztof G³ogocki <winthux@gmail.com>
2005-10-26 21:57:04 +0000

    bardzo wczesny początek socketów....
    git-svn-id: http://www.konnekt.info/svn/staminalib/branches/socket_winthux@122 e7532c55-18f9-0310-997f-f942ee4ad645

---
+++ Stamina/Socket.cpp
@@ -0,0 +1,102 @@
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2005 Krzysztof G³ogocki
+ *
+ *  $Id: $
+ */
+
+#include "stdafx.h"
+#include "Socket.h"
+
+namespace Stamina {
+
+	Socket::Socket(int major, int minor) 
+		: _threads(new ThreadRunner) {
+		WSAData data;
+		DWORD wVersionRequested;
+
+		wVersionRequested = MAKEWORD(major, minor);
+		WSAStartup(wVersionRequested, &wsaData);
+	}
+
+	String Socket::getHost() {
+		LockerCS lock(_critical);
+		return _host;
+	}
+
+	int Socket::getPort() {
+		LockerCS lock(_critical);
+		return _port;
+	}
+
+	State Socket::getState() {
+		LockerCS lock(_critical);
+		return _state;
+	}
+
+	bool Socket::Connect(const StringRef& host, int port) {
+		if (_state != stOffline)
+			return false;
+	
+		_host = host;
+		_port = port;
+		_state = stConnecting;
+
+		if ( _threads->runEx(boost::bind(&Socket::connecting, this)) ) {
+			onError(erInvalidThread);
+			return false
+		}
+		return true;
+	}
+
+	unsigned Socket::connecting() {
+		hostent *hp;
+		unsigned long addr;
+		struct sockaddr_in server;
+		unsigned long ul = 1;
+
+		_state = stConnecting;
+
+		_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, NULL, NULL);
+
+		if (_socket != INVALID_SOCKET)
+		{
+			if (inet_addr(_host.c_str()) == INADDR_NONE)
+				hp = gethostbyname(_host.c_str());
+			else {
+				addr = inet_addr(_host.c_str());
+				hp = gethostbyaddr((char*)&addr, sizeof(addr), AF_INET);
+			}
+
+			if (hp == NULL) {
+				closesocket(_socket);
+				onError(WSAGetLastError());
+			}
+			else {
+				server.sin_addr.s_addr =*((unsigned long*)hp->h_addr);
+				server.sin_family = AF_INET;
+				server.sin_port = htons( _port );
+
+				setsockopt(_socket,SOL_SOCKET,SO_KEEPALIVE,(const char*)&option,sizeof(option));
+				long option = 60*1000;
+				setsockopt(_socket, SOL_SOCKET, SO_RCVTIMEO,(char*)&option, sizeof(option));
+				setsockopt(_socket, SOL_SOCKET, SO_SNDTIMEO,(char*)&option, sizeof(option));
+				
+				if ((_event = WSACreateEvent()) == (HANDLE)-1 ||
+                    WSAEventSelect(_socket, _event, FD_ACCEPT|FD_CONNECT|FD_WRITE|FD_READ|FD_CLOSE) == SOCKET_ERROR ||
+                    connect(_socket, (const sockaddr_in*)&server, sizeof(sockaddr_in)) != SOCKET_ERROR ||
+					WSAGetLastError() != WSAEWOULDBLOCK ||
+					_threads->runEx(boost::bind(&Socket::loop, this)) != 0) {
+					closesocket(_socket);
+					onError(WSAGetLastError());
+				}
+			}
+		}
+		else
+			onError(WSAGetLastError());
+		return 0;
+	}
+};
---
+++ Stamina/Socket.h
@@ -0,0 +1,77 @@
+/*
+ *  Stamina.LIB
+ *  
+ *  Please READ /License.txt FIRST! 
+ * 
+ *  Copyright (C)2005 Krzysztof G³ogocki
+ *
+ *  $Id: $
+ */
+#ifndef __STAMINA_SOCKET__
+#define __STAMINA_SOCKET__
+
+#pragma once
+#include <winsock2.h>
+#pragma comment(lib, "Ws2_32")
+#include <boost/bind.hpp>
+#include <boost/signal.hpp>
+
+#include "iObject.h"
+#include "ThreadRun.h"
+#include "CrtiticalSection.h"
+#include "Exception.h"
+
+namespace Stamin {
+
+	class iSocket: public iObject {
+	public:
+		STAMINA_OBJECT_CLASS_VERSION(Stamina::iSocket, iObject, Version(0,1,0,0));
+		
+		virtual bool Send(const void* data, int size) = 0;
+	};
+
+	class Socket: public iSocket {
+	public:
+		STAMINA_OBJECT_CLASS_VERSION(Stamina::Socket, iSocket, Version(0,1,0,0));
+
+		Socket(int major = 2, int minor = 2);
+
+		virtual ~Socket() {
+			WSACleanup();
+		}
+		
+		enum State {
+			stOffline,
+			stConnecting,
+			stConnected,
+			stDisconnecting
+		};
+
+		enum Error {
+			erNone,
+			erInvalidThread		// Can't create thread
+		};
+	public:
+		bool Connect(const StringRef& host, int port);
+
+		String getHost() const;
+		int getPort() const;
+		State getState() const;
+	public:
+		boost::signal1<void, int> onError;
+	protected:
+		HANDLE _event;
+		ThreadRunnerStore* _threads;
+		State _state;
+		CriticalSection_w32 _critical;
+	private:
+		unsigned connecting();
+		unsigned loop();
+	private:
+		String _host;
+		int _port;
+		SOCKET _socket; 
+	};
+};
+
+#endif	// __STAMINA_SOCKET__
