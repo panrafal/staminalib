667bf35eda7405ba77eb093b8fe22bf4ae8cd942
Rafal Lindemann <rl@stamina.pl>
2006-01-29 13:54:08 +0000

    Zmiana licencji na MozPL dla całej biblioteki
    git-svn-id: http://www.konnekt.info/svn/staminalib/trunk@160 e7532c55-18f9-0310-997f-f942ee4ad645

--- Stamina/iArray.h
+++ Stamina/iArray.h
@@ -1,12 +1,28 @@
 /*
- *  Stamina.LIB
- *  
- *  Please READ /License.txt FIRST! 
- * 
- *  Copyright (C)2003,2004,2005 Rafa³ Lindemann, Stamina
- *
- *  $Id: $
- */
+
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License from
+/LICENSE.HTML in this package or at http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS"
+basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+License for the specific language governing rights and limitations
+under the License.
+
+The Original Code is "Stamina.lib" library code, released Feb 1, 2006.
+
+The Initial Developer of the Original Code is "STAMINA" - Rafa³ Lindemann.
+Portions created by STAMINA are 
+Copyright (C) 2003-2006 "STAMINA" - Rafa³ Lindemann. All Rights Reserved.
+
+Contributor(s): 
+
+--
+
+$Id: $
+
+*/
 
 #ifndef __STAMINA_IARRAY__
 #define __STAMINA_IARRAY__
